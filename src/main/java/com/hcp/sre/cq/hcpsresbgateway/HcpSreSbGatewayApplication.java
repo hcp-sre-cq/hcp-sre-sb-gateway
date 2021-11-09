0894267158e28cb2d8129620e9907da1d30c69f6
Hyungsoon Kim <noisonnoiton@gmail.com>
2021-11-09 17:57:49 +0900

    first commit

---
+++ src/main/java/com/hcp/sre/cq/hcpsresbgateway/HcpSreSbGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hcp.sre.cq.hcpsresbgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcpSreSbGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcpSreSbGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcp/sre/cq/hcpsresbgateway/HcpSreSbGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcp.sre.cq.hcpsresbgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcpSreSbGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
